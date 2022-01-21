ae9e598769fa21e40fad68c2daa487a3d451478e
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-01-21 13:38:40 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop jasmin
-net start jasmin
+net stop Jasmin
+net start Jasmin
